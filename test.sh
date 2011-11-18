3154d1d2a49db19b245a0609a861781f203db379
Visti Kløft <hr.kloft@gmail.com>
2011-11-18 01:30:22 +0100

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,26 @@
-sudo /var/www/lib/Cake/Console/cake testsuite core Basics --log-junit /var/www/app/webroot/test/basics.xml
+
+gettest()
+{
+	sudo /var/www/lib/Cake/Console/cake testsuite core $1 --log-junit /var/www/app/webroot/test/$1.xml
+}
+
+gettest Basics
+gettest AllBehaviors
+gettest AllCache
+gettest AllComponents
+gettest AllConfigure
+gettest AllConsole
+gettest AllController
+gettest AllCore
+gettest AllDatabase
+gettest AllError
+gettest AllHelpers
+gettest AllI18n
+gettest AllLog
+gettest AllModel
+gettest AllNetwork
+gettest AllRouting
+gettest AllTestSuite
+gettest AllTests
+gettest AllUtility
+gettest AllView
